f915939b7b04f2aa0d6fce58a208b3e4a6d8a299
Eric Albertus <ERAlchemist@gmail.com>
2018-10-10 23:25:00 -0700

    Fun With Operators

---
+++ Practice/src/WaterTank.java
@@ -0,0 +1,10 @@
+public class WaterTank {
+    private String brand;
+    private boolean empty;
+
+    public static void main(String[] args) {
+        WaterTank wb = new WaterTank();
+        System.out.println("Empty= " + wb.empty);
+        System.out.println(", Brand= " + wb.brand);
+    }
+}
---
+++ Practice/src/com/ERAlbertus/Main.java
@@ -0,0 +1,9 @@
+package com.ERAlbertus;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+    }
+}
